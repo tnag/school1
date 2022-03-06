80b277bfdaaa039a57f802e4c3268a3fbb9323c4
tnag <tirthankarnag2007@gmail.com>
2022-03-06 07:04:37 +0530

    //feat-3 non-boundary means not all sides are open

--- src/edu/ghs11/project1/SortNonBoundary.java
+++ src/edu/ghs11/project1/SortNonBoundary.java
@@ -2,7 +2,7 @@ package edu.ghs11.project1;
 
 import java.util.Scanner;
 
-//feat-3 no sides are open
+//feat-3 non-boundary means not all sides are open
 public class SortNonBoundary {
 
 	int m=0;
